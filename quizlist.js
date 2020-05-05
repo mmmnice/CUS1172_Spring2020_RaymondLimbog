99a4a8778760360f9fb11b812e2cbfed6631dceb
Raymond Limbog <ray@Raymonds-Air.home>
2020-05-04 22:09:29 -0400

    quizlist

--- quizlist.js
+++ quizlist.js
@@ -2,12 +2,12 @@ var quiz_list={
     1: {
         id:"1",
         title: "quiz 1",
-        description: "Sample text"
+        description: "Hey this is the first quiz for this single page application project"
     },
     2: {
         id : "2",
         title: "quiz 2",
-        description:"Sample text 2"
+        description:"Hey this is the second quiz for this single page application project"
     }
 };
 
